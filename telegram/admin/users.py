8b4e0aab3e7b75d34da34201c67d9950de341c8a
Eldos <eldos.baktybekov@gmail.com>
2024-01-19 23:23:21 +0600

    :sparkles: Add user model admin features

--- telegram/admin/users.py
+++ telegram/admin/users.py
@@ -7,4 +7,27 @@ __all__ = ('UserAdmin',)
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    pass
+    search_fields = (
+        'id',
+        'username',
+        'first_name',
+        'last_name',
+    )
+    search_help_text = 'You can search by id, username, first name, last name'
+    list_display = (
+        'id',
+        'username',
+        'first_name',
+        'last_name',
+        'created_at',
+    )
+    list_display_links = (
+        'id',
+        'username',
+        'first_name',
+        'last_name',
+    )
+    ordering = ('-created_at',)
+
+    def get_readonly_fields(self, request, obj=None):
+        return ('id',) if obj else ()
