faf69d3589a39d8e4c6a21d63b6bd471ec387c13
Eldos <eldos.baktybekov@gmail.com>
2024-01-20 17:45:10 +0600

    :card_file_box: Add `start_text` field in `Bot` model

--- telegram/models/bots.py
+++ telegram/models/bots.py
@@ -6,6 +6,7 @@ __all__ = ('Bot',)
 class Bot(models.Model):
     token = models.CharField(max_length=255)
     name = models.CharField(max_length=255)
+    start_text = models.TextField(max_length=4096, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
