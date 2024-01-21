68b19ff3bfcf0379b4489fb71c5632d18f9a602c
Eldos <eldos.baktybekov@gmail.com>
2024-01-21 23:18:14 +0600

    :card_file_box: Add `SaleTemporaryCode` model

--- shops/models/__init__.py
+++ shops/models/__init__.py
@@ -1,5 +1,7 @@
+from .sale_temporary_codes import *
 from .shop_admins import *
 from .shop_clients import *
+from .shop_groups import *
 from .shop_sales import *
 from .shop_salesmans import *
 from .shops import *
---
+++ shops/models/sale_temporary_codes.py
@@ -0,0 +1,37 @@
+import random
+import string
+from datetime import timedelta, datetime
+
+from django.conf import settings
+from django.db import models
+
+from shops.models.shop_clients import ShopClient
+from shops.models.shop_groups import ShopGroup
+
+__all__ = ('SaleTemporaryCode',)
+
+
+def generate_code() -> str:
+    return ''.join(random.choices(string.digits, k=4))
+
+
+class SaleTemporaryCode(models.Model):
+    client = models.ForeignKey(
+        to=ShopClient,
+        on_delete=models.CASCADE,
+    )
+    group = models.ForeignKey(
+        to=ShopGroup,
+        on_delete=models.CASCADE,
+    )
+    code = models.CharField(max_length=4, default=generate_code)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('client', 'group', 'code')
+
+    @property
+    def expires_at(self) -> datetime:
+        return self.created_at + timedelta(
+            seconds=settings.SALE_TEMPORARY_CODE_LIFETIME_SECONDS,
+        )
