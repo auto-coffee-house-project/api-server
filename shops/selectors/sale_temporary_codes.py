df2e61942887551fa0634908fbe6ebc21932365c
Eldos <eldos.baktybekov@gmail.com>
2024-01-23 14:17:24 +0600

    :necktie: Add expired codes selector

--- shops/selectors/sale_temporary_codes.py
+++ shops/selectors/sale_temporary_codes.py
@@ -1,7 +1,16 @@
+from datetime import timedelta
+
+from django.conf import settings
+from django.db.models import QuerySet
+from django.utils import timezone
+
 from core.exceptions import ObjectDoesNotExistError
 from shops.models import SaleTemporaryCode
 
-__all__ = ('get_sale_temporary_code',)
+__all__ = (
+    'get_sale_temporary_code',
+    'get_expired_sale_temporary_codes',
+)
 
 
 def get_sale_temporary_code(code: str) -> SaleTemporaryCode:
@@ -12,3 +21,11 @@ def get_sale_temporary_code(code: str) -> SaleTemporaryCode:
             f'Sale temporary code by {code=} does not exist',
             code=code,
         )
+
+
+def get_expired_sale_temporary_codes() -> QuerySet[SaleTemporaryCode]:
+    now = timezone.now()
+    codes_expire_at = now - timedelta(
+        seconds=settings.SALE_TEMPORARY_CODE_LIFETIME_SECONDS,
+    )
+    return SaleTemporaryCode.objects.filter(created_at__lt=codes_expire_at)
