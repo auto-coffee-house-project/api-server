e08340cc54bb57c229e128c09195fcf61d42dec7
Eldos <eldos.baktybekov@gmail.com>
2024-01-23 22:18:15 +0600

    :recycle: Refactor generic API view to raw API view

--- shops/views/shop_salesmans.py
+++ shops/views/shop_salesmans.py
@@ -1,11 +1,26 @@
-from rest_framework.generics import RetrieveAPIView
+from rest_framework import serializers
+from rest_framework.exceptions import NotFound
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-from shops.models import ShopSalesman
-from shops.serializers import ShopSalesmanSerializer
+from core.exceptions import ObjectDoesNotExistError
+from shops.selectors import get_shop_salesman_by_user_id
+
+__all__ = ('ShopSalesmanRetrieveApi',)
 
 
-class ShopSalesmanRetrieveApi(RetrieveAPIView):
-    queryset = ShopSalesman.objects.all()
-    serializer_class = ShopSalesmanSerializer
-    lookup_url_kwarg = 'salesman_user_id'
-    lookup_field = 'user_id'
+class ShopSalesmanRetrieveApi(APIView):
+
+    class OutputSerializer(serializers.Serializer):
+        id = serializers.IntegerField()
+        user_id = serializers.IntegerField()
+
+    def get(self, request: Request, user_id: int) -> Response:
+        try:
+            salesman = get_shop_salesman_by_user_id(user_id)
+        except ObjectDoesNotExistError as error:
+            raise NotFound(error.message)
+        serializer = self.OutputSerializer(salesman)
+        response_data = {'ok': True, 'result': serializer.data}
+        return Response(response_data)
