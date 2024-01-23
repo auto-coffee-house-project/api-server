29bbca3202db2859c3b582963ad1b700bebcf477
Eldos <eldos.baktybekov@gmail.com>
2024-01-23 14:20:31 +0600

    :zap: Add related objects prefetch

--- shops/selectors/shop_salesmans.py
+++ shops/selectors/shop_salesmans.py
@@ -6,7 +6,11 @@ __all__ = ('get_shop_salesman_by_user_id',)
 
 def get_shop_salesman_by_user_id(user_id: int) -> ShopSalesman:
     try:
-        return ShopSalesman.objects.select_related('shop').get(user_id=user_id)
+        return (
+            ShopSalesman.objects
+            .select_related('shop', 'shop__group')
+            .get(user_id=user_id)
+        )
     except ShopSalesman.DoesNotExist:
         raise ObjectDoesNotExistError(
             f'Shop salesman by {user_id=} does not exist',
