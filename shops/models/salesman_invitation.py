845c9fe2a4cc69a1f6c1789d25c480d9bf175eac
Eldos <eldos.baktybekov@gmail.com>
2024-01-24 17:17:41 +0600

    :card_file_box: Add database model

---
+++ shops/models/salesman_invitation.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+from uuid import uuid4
+
+from django.conf import settings
+from django.db import models
+
+from shops.models.shop_admins import ShopAdmin
+from shops.models.shops import Shop
+
+__all__ = ('SalesmanInvitation',)
+
+
+class SalesmanInvitation(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    shop_id = models.ForeignKey(
+        to=Shop,
+        on_delete=models.CASCADE,
+    )
+    created_by_admin = models.ForeignKey(
+        to=ShopAdmin,
+        on_delete=models.CASCADE,
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def expires_at(self) -> datetime:
+        return self.created_at + settings.SALESMAN_INVITATION_LIFETIME_SECONDS
