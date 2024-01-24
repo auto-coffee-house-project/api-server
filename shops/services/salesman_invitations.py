90624c828089b66511c9d3241b05fa2efdb2a97a
Eldos <eldos.baktybekov@gmail.com>
2024-01-24 18:53:22 +0600

    :necktie: Add invitation creation service

--- shops/services/salesman_invitations.py
+++ shops/services/salesman_invitations.py
@@ -6,5 +6,5 @@ __all__ = ('create_salesman_invitation',)
 def create_salesman_invitation(shop_admin: ShopAdmin) -> SalesmanInvitation:
     return SalesmanInvitation.objects.create(
         shop_id=shop_admin.shop_id,
-        created_by_admin_id=shop_admin.user_id,
+        created_by_admin=shop_admin,
     )
