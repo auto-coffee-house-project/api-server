3a2ab632f116d6e93ff548ee34af7ca53f63da11
Eldos <eldos.baktybekov@gmail.com>
2024-01-26 01:16:02 +0600

    :pencil2: Fix project name

--- coffee_house/celery.py
+++ coffee_house/celery.py
@@ -2,7 +2,7 @@ import os
 
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vobla_vpn.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coffee_house.settings')
 
 app = Celery('coffee_house')
 
