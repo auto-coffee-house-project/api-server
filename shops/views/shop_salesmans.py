7d28a96900a1a0d344596fd08c2f3828011ed233
Eldos <eldos.baktybekov@gmail.com>
2024-01-26 01:06:36 +0600

    :sparkles: Add API endpoint for salesman creation

--- shops/views/shop_salesmans.py
+++ shops/views/shop_salesmans.py
@@ -1,11 +1,20 @@
+from uuid import UUID
+
 from rest_framework import serializers
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from shops.selectors import get_shop_salesman_by_user_id
+from shops.selectors import (
+    get_shop_salesman_by_user_id,
+    get_salesman_invitation_by_id,
+)
+from shops.services.shop_salesmans import create_salesman_by_invitation
 
-__all__ = ('ShopSalesmanRetrieveApi',)
+__all__ = (
+    'ShopSalesmanRetrieveApi',
+    'ShopSalesmanCreateApi',
+)
 
 
 class ShopSalesmanRetrieveApi(APIView):
@@ -17,5 +26,34 @@ class ShopSalesmanRetrieveApi(APIView):
     def get(self, request: Request, user_id: int) -> Response:
         salesman = get_shop_salesman_by_user_id(user_id)
         serializer = self.OutputSerializer(salesman)
+        response_data = {'ok': True, 'result': serializer.data}
+        return Response(response_data)
+
+
+class ShopSalesmanCreateApi(APIView):
+
+    class InputSerializer(serializers.Serializer):
+        user_id = serializers.IntegerField()
+        invitation_id = serializers.UUIDField()
+
+    class OutputSerializer(serializers.Serializer):
+        shop_name = serializers.CharField(source='shop.name')
+        shop_group_name = serializers.CharField(source='shop.group.name')
+
+    def post(self, request: Request) -> Response:
+        serializer = self.InputSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serialized_data = serializer.data
+
+        user_id: int = serialized_data['user_id']
+        invitation_id: UUID = serialized_data['invitation_id']
+
+        invitation = get_salesman_invitation_by_id(invitation_id)
+        create_salesman_by_invitation(
+            user_id=user_id,
+            invitation=invitation,
+        )
+
+        serializer = self.OutputSerializer(invitation)
         response_data = {'ok': True, 'result': serializer.data}
         return Response(response_data)
