0bc8bff6b3366b8c7e43744325618982cc1a812c
Eldos <eldos.baktybekov@gmail.com>
2024-01-27 12:37:44 +0600

    :zap: Add select related field

--- shops/selectors/shop_clients.py
+++ shops/selectors/shop_clients.py
@@ -6,6 +6,6 @@ __all__ = ('get_shop_client_by_user_id',)
 
 def get_shop_client_by_user_id(user_id: int) -> ShopClient:
     try:
-        return ShopClient.objects.get(user_id=user_id)
+        return ShopClient.objects.select_related('user').get(user_id=user_id)
     except ShopClient.DoesNotExist:
         raise ObjectDoesNotExistError({'user_id': user_id})
