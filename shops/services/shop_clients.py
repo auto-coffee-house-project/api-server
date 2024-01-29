0ade9cc93491f31ba69304bcc3fa9bbebce2f6bb
Eldos <eldos.baktybekov@gmail.com>
2024-01-29 10:06:17 +0600

    :necktie: Add service function for retrieving shop client statistics

--- shops/services/shop_clients.py
+++ shops/services/shop_clients.py
@@ -1,6 +1,8 @@
-from shops.models import ShopClient
+from shops.models import ShopClient, ShopGroup, ShopClientStatistics
 
-__all__ = ('get_or_create_shop_client',)
+from shops.selectors import count_client_purchases_in_shop_group
+
+__all__ = ('get_or_create_shop_client', 'get_shop_client_statistics')
 
 
 def get_or_create_shop_client(user_id: int) -> tuple[ShopClient, bool]:
@@ -8,3 +10,22 @@ def get_or_create_shop_client(user_id: int) -> tuple[ShopClient, bool]:
         return ShopClient.objects.get(user_id=user_id), False
     except ShopClient.DoesNotExist:
         return ShopClient.objects.create(user_id=user_id), True
+
+
+def get_shop_client_statistics(
+        shop_client: ShopClient,
+        shop_group: ShopGroup,
+) -> ShopClientStatistics:
+    total_purchases_count = count_client_purchases_in_shop_group(
+        client_id=shop_client.id,
+        shop_group_id=shop_group.id,
+    )
+    current_cups_count = total_purchases_count % shop_group.each_nth_cup_free
+
+    return ShopClientStatistics(
+        user_id=shop_client.user.id,
+        shop_group_bot_id=shop_group.bot.id,
+        purchases_count=total_purchases_count,
+        each_nth_cup_free=shop_group.each_nth_cup_free,
+        current_cups_count=current_cups_count,
+    )
