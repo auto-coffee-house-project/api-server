60f4e7bdcf407bee6efcfdb9619b27134a29db78
Eldos <eldos.baktybekov@gmail.com>
2024-02-01 22:00:55 +0600

    :sparkles: Add exception class

--- shops/exceptions/shop_salesmans.py
+++ shops/exceptions/shop_salesmans.py
@@ -2,9 +2,14 @@ from rest_framework import status
 
 from core.exceptions import ApplicationError
 
-__all__ = ('ShopSalesmanAlreadyExistsError',)
+__all__ = ('ShopSalesmanAlreadyExistsError', 'UserAlreadyShopAdminError')
 
 
 class ShopSalesmanAlreadyExistsError(ApplicationError):
     default_code = 'User is already salesman'
+    status_code = status.HTTP_409_CONFLICT
+
+
+class UserAlreadyShopAdminError(ApplicationError):
+    default_code = 'User is already shop admin'
     status_code = status.HTTP_409_CONFLICT
