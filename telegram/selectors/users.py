e072a01435cd9b4ae9a2341d020efa515a57d468
Eldos <eldos.baktybekov@gmail.com>
2024-02-01 21:50:26 +0600

    :necktie: Update user role selector

--- telegram/selectors/users.py
+++ telegram/selectors/users.py
@@ -1,24 +1,27 @@
 from typing import Literal
 
 from core.exceptions import ObjectDoesNotExistError
-from shops.selectors import (
-    get_shop_admin_by_user_id,
-    get_shop_salesman_by_user_id,
-)
+from shops.selectors import is_shop_admin, is_shop_salesman
+from telegram.models import User
 
-__all__ = ('get_user_role',)
+__all__ = ('get_user_role', 'get_user_by_id')
 
 
-def get_user_role(user_id: int) -> Literal['client', 'salesman', 'admin']:
+def get_user_role(
+        user_id: int | type[int],
+        shop_group_id: int | type[int],
+) -> Literal['client', 'salesman', 'admin']:
+    if is_shop_admin(user_id=user_id, shop_group_id=shop_group_id):
+        return 'admin'
+
+    if is_shop_salesman(user_id=user_id, shop_group_id=shop_group_id):
+        return 'salesman'
+
+    return 'client'
+
+
+def get_user_by_id(user_id: int) -> User:
     try:
-        get_shop_admin_by_user_id(user_id)
-    except ObjectDoesNotExistError:
-        try:
-            get_shop_salesman_by_user_id(user_id)
-        except ObjectDoesNotExistError:
-            role: Literal['client'] = 'client'
-        else:
-            role: Literal['salesman'] = 'salesman'
-    else:
-        role: Literal['admin'] = 'admin'
-    return role
+        return User.objects.get(id=user_id)
+    except User.DoesNotExist:
+        raise ObjectDoesNotExistError({'user_id': user_id})
