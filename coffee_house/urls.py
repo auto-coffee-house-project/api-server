8d113baedd2bd96d087db6e5866913f98cb02031
usbtypec <eldos.baktybekov@gmail.com>
2024-02-07 23:36:50 +0600

    :sparkles: Add ping endpoint

--- coffee_house/urls.py
+++ coffee_house/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('core.urls')),
     path('api/shops/', include('shops.urls')),
     path('api/telegram/', include('telegram.urls')),
 ]
--- core/views.py
+++ core/views.py
@@ -1,13 +1,20 @@
 from django.core import exceptions as django_exceptions
 from django.http import Http404
 from rest_framework import exceptions as drf_exceptions, status
+from rest_framework.decorators import api_view
+from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.serializers import as_serializer_error
 from rest_framework.views import exception_handler as drf_exception_handler
 
 from core.exceptions import ApplicationError, ObjectDoesNotExistError
 
-__all__ = ('exception_handler',)
+__all__ = ('exception_handler', 'ping_view')
+
+
+@api_view(['GET'])
+def ping_view(request: Request) -> Response:
+    return Response({'ok': True, 'result': {'message': 'Pong!'}})
 
 
 def exception_handler(exc, context) -> Response | None:
