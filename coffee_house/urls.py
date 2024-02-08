efc0f3eff278a0d2cd634b3164aec3490e9972d0
usbtypec <eldos.baktybekov@gmail.com>
2024-02-08 17:53:56 +0600

    :boom: Change admin panel url

--- coffee_house/urls.py
+++ coffee_house/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('api/admin/', admin.site.urls),
     path('api/', include('core.urls')),
     path('api/shops/', include('shops.urls')),
     path('api/telegram/', include('telegram.urls')),
