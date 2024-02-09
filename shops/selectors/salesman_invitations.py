a912f55474f1995210efd6e21a6faa65cf93c0eb
usbtypec <eldos.baktybekov@gmail.com>
2024-02-09 11:35:12 +0600

    :sparkles: Add tasks for cleaning up expired invitations

--- shops/selectors/salesman_invitations.py
+++ shops/selectors/salesman_invitations.py
@@ -1,9 +1,14 @@
+from datetime import timedelta
 from uuid import UUID
+
+from django.conf import settings
+from django.db.models import QuerySet
+from django.utils import timezone
 
 from core.exceptions import ObjectDoesNotExistError
 from shops.models import SalesmanInvitation
 
-__all__ = ('get_salesman_invitation_by_id',)
+__all__ = ('get_salesman_invitation_by_id', 'get_expired_salesman_invitations')
 
 
 def get_salesman_invitation_by_id(
@@ -17,3 +22,11 @@ def get_salesman_invitation_by_id(
         )
     except SalesmanInvitation.DoesNotExist:
         raise ObjectDoesNotExistError({'invitation_id': invitation_id})
+
+
+def get_expired_salesman_invitations() -> QuerySet[SalesmanInvitation]:
+    now = timezone.now()
+    expires_at = now - timedelta(
+        seconds=settings.SALESMAN_INVITATION_LIFETIME_SECONDS,
+    )
+    return SalesmanInvitation.objects.filter(created_at__lt=expires_at)
