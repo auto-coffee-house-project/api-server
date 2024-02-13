6c2f650443a8fbc36f8689995a4c26665f0fe615
Eldos <eldos.baktybekov@gmail.com>
2024-02-13 11:29:07 +0600

    :safety_vest: Add authentication and authorization in shop product list/create API endpoint

--- shops/views/shop_products.py
+++ shops/views/shop_products.py
@@ -5,19 +5,23 @@ from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from shops.selectors import get_shop_group_by_bot_id
 from shops.services.shop_products import create_shop_product
+from telegram.authentication import BotAuthentication
+from telegram.models import Bot
+from telegram.permissions import HasBot
 
-__all__ = ('ShopProductCreateApi',)
+__all__ = ('ShopProductListCreateApi',)
 
 
-class ShopProductCreateApi(APIView):
+class ShopProductListCreateApi(APIView):
+    authentication_classes = [BotAuthentication]
+    permission_classes = [HasBot]
+
     class InputSerializer(serializers.Serializer):
         class PhotoSerializer(serializers.Serializer):
             url = serializers.URLField()
             is_main = serializers.BooleanField()
 
-        shop_group_bot_id = serializers.IntegerField()
         name = serializers.CharField(max_length=100)
         price = serializers.DecimalField(max_digits=10, decimal_places=2)
         category_ids = serializers.ListField(child=serializers.IntegerField())
@@ -45,16 +49,15 @@ class ShopProductCreateApi(APIView):
 
         name: str = serialized_data['name']
         price: Decimal = serialized_data['price']
-        shop_group_bot_id: int = serialized_data['shop_group_bot_id']
         category_ids: set[int] = set(serialized_data['category_ids'])
         photos: list[dict[str, any]] = serialized_data['photos']
 
-        shop_group = get_shop_group_by_bot_id(shop_group_bot_id)
+        bot: Bot = request.META['bot']
 
         product = create_shop_product(
             name=name,
             price=price,
-            shop_group=shop_group,
+            shop_group=bot.shopgroup,
             category_ids=category_ids,
             photos=photos,
         )
