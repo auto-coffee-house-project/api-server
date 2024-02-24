35742a27132e2bd8598c7c509be41213fa2191c8
Eldos <eldos.baktybekov@gmail.com>
2024-02-24 14:27:34 +0600

    :necktie: Add shop client user ids selector

--- shops/selectors/shop_clients.py
+++ shops/selectors/shop_clients.py
@@ -1,7 +1,7 @@
 from core.exceptions import ObjectDoesNotExistError
 from shops.models import ShopClient
 
-__all__ = ('get_shop_client',)
+__all__ = ('get_shop_client', 'get_shop_client_user_ids')
 
 
 def get_shop_client(user_id: int, shop_id: int) -> ShopClient:
@@ -13,3 +13,9 @@ def get_shop_client(user_id: int, shop_id: int) -> ShopClient:
         )
     except ShopClient.DoesNotExist:
         raise ObjectDoesNotExistError({'user_id': user_id})
+
+
+def get_shop_client_user_ids(shop_id: int) -> set[int]:
+    shop_clients = ShopClient.objects.filter(shop_id=shop_id)
+    user_ids = shop_clients.values_list('user_id', flat=True)
+    return set(user_ids)
