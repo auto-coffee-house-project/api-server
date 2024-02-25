45bda38b88d60d91a185777b42bfff302d0f6e09
Eldos <eldos.baktybekov@gmail.com>
2024-02-25 18:13:12 +0600

    :card_file_box: Add bot phrases in Bot model

--- telegram/models/bots.py
+++ telegram/models/bots.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import CheckConstraint
 
 __all__ = ('Bot',)
 
@@ -9,7 +10,30 @@ class Bot(models.Model):
     name = models.CharField(max_length=255)
     username = models.CharField(max_length=255)
     start_text = models.TextField(max_length=4096)
+    sale_created_text = models.TextField(
+        max_length=4096,
+        default=(
+            '✅ Код успешно применен!\n'
+            '🔥 Для получения подарка осталось'
+            ' совершить ещё {count} покупок'
+        ),
+    )
+    gift_given_text = models.TextField(
+        max_length=4096,
+        default='🎉 Поздравляем! Вы получили подарок!',
+    )
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        constraints = (
+            CheckConstraint(
+                name='bot_sale_created_text_contains_count',
+                check=models.Q(sale_created_text__contains='{count}'),
+                violation_error_message=(
+                    'Sale created text must contain "{count}"'
+                ),
+            ),
+        )
