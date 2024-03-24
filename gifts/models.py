af69102b815eaaff4a47996b6b91882a641e3825
Eldos <eldos.baktybekov@gmail.com>
2024-03-24 14:05:31 +0600

    :card_file_box: Change Gift model
    - Make shop and code unique together

--- gifts/models.py
+++ gifts/models.py
@@ -1,3 +1,5 @@
+import random
+import string
 from uuid import uuid4
 
 from django.db import models
@@ -8,9 +10,13 @@ from shops.models.shops import Shop
 __all__ = ('Gift',)
 
 
+def generate_code() -> str:
+    return random.choices(string.digits, k=4)
+
+
 class Gift(models.Model):
     id = models.UUIDField(primary_key=True, editable=False, default=uuid4)
-    code = models.CharField(max_length=4, unique=True)
+    code = models.CharField(max_length=4, db_index=True, default=generate_code)
     client = models.ForeignKey(
         to=ShopClient,
         on_delete=models.CASCADE,
@@ -21,3 +27,7 @@ class Gift(models.Model):
     )
     created_at = models.DateTimeField(auto_now_add=True)
     expires_at = models.DateTimeField()
+
+    class Meta:
+        db_table = 'gifts'
+        unique_together = ('shop', 'code')
